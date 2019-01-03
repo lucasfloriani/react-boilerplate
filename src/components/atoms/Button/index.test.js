e9815210b039e9b83d47205141cb22bcce3cae5b
lucasfloriani <lucas.floriane@gmail.com>
2019-01-03 17:02:04 -0200

    Correct button component and test

--- src/components/atoms/Button/index.test.js
+++ src/components/atoms/Button/index.test.js
@@ -1,38 +1,54 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
+import { BrowserRouter } from 'react-router-dom'
 import Button from '.'
 
-const wrap = (props = {}) => shallow(<Button {...props} />).dive()
+const wrapWithShallow = (props = {}) => shallow(<Button {...props} />)
+const wrapWithMount = (props = {}) => mount(<Button {...props} />)
 
 describe('<Button />', () => {
-  it('renders with different combination of props', () => {
-    wrap({ disabled: true })
-    wrap({ transparent: true })
-    wrap({ disabled: true, transparent: true })
+  it('renders with disable', () => {
+    const wrapper = wrapWithMount({ disabled: true })
+    expect(wrapper.find({ disabled: true }))
+  })
+
+  it('renders with transparent effect', () => {
+    const wrapper = wrapWithMount({ transparent: true })
+    expect(wrapper.find({ transparent: true }))
+  })
+
+  it('renders with disable and transparent props', () => {
+    const wrapper = wrapWithMount({ disabled: true, transparent: true })
+    expect(wrapper.find({ disabled: true, transparent: true }))
   })
 
   it('renders children when passed in', () => {
-    const wrapper = wrap({ children: 'test' })
+    const wrapper = wrapWithShallow({ children: 'test' })
     expect(wrapper.contains('test')).toBe(true)
   })
 
   it('renders props when passed in', () => {
-    const wrapper = wrap({ type: 'submit' })
+    const wrapper = wrapWithShallow({ type: 'submit' })
     expect(wrapper.find({ type: 'submit' })).toHaveLength(1)
   })
 
   it('renders button by default', () => {
-    const wrapper = wrap()
+    const wrapper = wrapWithMount()
     expect(wrapper.find('button')).toHaveLength(1)
   })
 
   it('renders anchor when href is passed in', () => {
-    const wrapper = wrap({ href: 'test' })
+    const wrapper = wrapWithMount({ href: 'test' })
     expect(wrapper.find('a')).toHaveLength(1)
   })
 
   it('renders Link when to is passed in', () => {
-    const wrapper = wrap({ to: 'test' }).dive()
+    const props = { to: 'test' }
+    const wrapper = mount(
+      <BrowserRouter>
+        <Button {...props} />
+      </BrowserRouter>
+    )
     expect(wrapper.find('Link')).toHaveLength(1)
   })
 })

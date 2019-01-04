748b934f6fe0c12236142b52f6d77c6be3ac9610
lucasfloriani <lucas.floriane@gmail.com>
2019-01-04 09:03:11 -0200

    Correct Heading test

--- src/components/atoms/Heading/index.test.js
+++ src/components/atoms/Heading/index.test.js
@@ -1,27 +1,28 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import Heading from '.'
 
-const wrap = (props = {}) => shallow(<Heading {...props} />).dive()
+const wrapWithShallow = (props = {}) => shallow(<Heading {...props} />)
+const wrapWithMount = (props = {}) => mount(<Heading {...props} />)
 
 describe('<Heading />', () => {
   it('renders children when passed in', () => {
-    const wrapper = wrap({ children: 'test' })
+    const wrapper = wrapWithShallow({ children: 'test' })
     expect(wrapper.contains('test')).toBe(true)
   })
 
   it('renders props when passed in', () => {
-    const wrapper = wrap({ id: 'foo' })
+    const wrapper = wrapWithShallow({ id: 'foo' })
     expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
   })
 
   it('renders h1 by default', () => {
-    const wrapper = wrap()
+    const wrapper = wrapWithMount()
     expect(wrapper.find('h1')).toHaveLength(1)
   })
 
   it('renders hLevel when level is passed in', () => {
-    const wrapper = wrap({ level: 2 })
+    const wrapper = wrapWithMount({ level: 2 })
     expect(wrapper.find('h2')).toHaveLength(1)
   })
 })
